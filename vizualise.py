06be5bc8fe4c32bc064f56716ab1ff32e9228351
martingeew <67925955+martingeew@users.noreply.github.com>
2024-10-27 01:05:38 +0200

    Update color map and add legend

--- vizualise.py
+++ vizualise.py
@@ -2,6 +2,7 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from pypalettes import load_cmap
+import matplotlib.patches as mpatches
 
 
 # Function to annotate states
@@ -22,14 +23,13 @@ def annotate_states(geo_df, ax, value_col):
         )
 
 
-def turn_on_spines(ax, spine_width=1):
+def set_background_color(ax, color="#f5f5f5"):
     """
-    Turn on the spines for a given axes object after the axis has been turned off.
-    Optionally, set the width of the spines.
+    Sets the background color of a given subplot.
 
     Parameters:
-    - ax: The axes object for which to turn on the spines.
-    - spine_width: The width of the spines (default is 1).
+    - ax: Matplotlib axis object on which to set the background color.
+    - color: String, color in hex format to set as the background. Default is slight off-white (#f5f5f5).
     """
     # Turn the axis back on, but this also restores labels and ticks
     ax.set_axis_on()
@@ -37,16 +37,11 @@ def turn_on_spines(ax, spine_width=1):
     # Remove the x and y axis ticks while keeping the spines
     ax.set_xticks([])
     ax.set_yticks([])
+    ax.set_facecolor(color)
 
-    # Turn on the spines for all sides
-    ax.spines["top"].set_visible(True)
-    ax.spines["bottom"].set_visible(True)
-    ax.spines["left"].set_visible(True)
-    ax.spines["right"].set_visible(True)
-
-    # Set the border (spine) width
+    # Remove the spines
     for spine in ax.spines.values():
-        spine.set_linewidth(spine_width)
+        spine.set_visible(False)
 
 
 employment_data = pd.read_csv(
@@ -73,23 +68,38 @@ print(states_not_in_intersect)
 
 # Choropleth
 
-# Load colormap
-cmap = load_cmap("Bmsurface")
 
-# Project the data to EPSG:5070
+# Define column for plotting
+column_to_plot = "apc_20240901"
+
+# Project the data to EPSG:5070 and calculate centroids
 data_projected = data.to_crs(epsg=5070)
 data_projected["centroid"] = data_projected.geometry.centroid
 
-# Project the centroids back to the original CRS for correct positioning in the plot
+# Project centroids back to original CRS
 data["centroid"] = data_projected["centroid"].to_crs(data.crs)
+
+# Add a binned column based on specified ranges
+data["binned"] = pd.cut(
+    data[column_to_plot],
+    bins=[0, 1, 2, 3, float("inf")],
+    labels=["0-1", "1-2", "2-3", "3+"],
+)
+
+# Define custom colors for each bin
+color_mapping = {
+    "0-1": "#05DBF2FF",
+    "1-2": "#05C7F2FF",
+    "2-3": "#05AFF2FF",
+    "3+": "#035AA6FF",
+}
 
 # Separate Alaska, Hawaii, and the contiguous U.S.
 alaska = data[data["NAME"] == "Alaska"]
 hawaii = data[data["NAME"] == "Hawaii"]
 contiguous_us = data[(data["NAME"] != "Alaska") & (data["NAME"] != "Hawaii")]
 
-# Set up a 2-row, 2-column grid with the main US plot spanning the entire top row,
-# and Alaska and Hawaii in the second row as 2 separate columns
+# Set up a 2x2 grid layout with custom size ratios
 new_width = 20 * 0.5
 new_height = 15 * 0.5
 fig, ax = plt.subplots(
@@ -100,54 +110,74 @@ fig, ax = plt.subplots(
     gridspec_kw={"height_ratios": [4, 1], "width_ratios": [1, 1]},
 )
 
-# Create the main US plot on the first row spanning both columns
+
+def plot_with_legend(data, ax, xlim, ylim):
+    """
+    Plots the data on the provided axis with optional legend.
+
+    Parameters:
+    - data: GeoDataFrame to plot.
+    - ax: Matplotlib axis to plot on.
+    - xlim: Tuple for x-axis limits.
+    - ylim: Tuple for y-axis limits.
+    - show_legend: Boolean, if True displays the legend; if False, hides the legend.
+    """
+    # Plot data with custom color mapping
+    data.plot(
+        ax=ax,
+        column="binned",
+        color=data["binned"].map(color_mapping),
+        edgecolor="white",
+        linewidth=0.5,
+        legend=False,  # Disable automatic legend
+    )
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+
+# Plot contiguous U.S. on the main subplot (spanning both columns in the first row)
 ax_main = plt.subplot2grid((2, 2), (0, 0), colspan=2, fig=fig)
-
-column_to_plot = "apc_20240901"
-
-# Plot the contiguous U.S. on the main subplot
-contiguous_us.plot(
-    ax=ax_main, column=column_to_plot, cmap=cmap, edgecolor="black", linewidth=0.5
-)
-
-# Set axis properties for main plot
-ax_main.set_xlim(-130, -65)
-ax_main.set_ylim(24, 55)
+plot_with_legend(contiguous_us, ax_main, xlim=(-130, -65), ylim=(24, 55))
 
 # Alaska plot in the second row, first column
 ax_alaska = plt.subplot2grid((2, 2), (1, 0), fig=fig)
-
-# Plot Alaska in the subplot
-alaska.plot(
-    ax=ax_alaska, column=column_to_plot, cmap=cmap, edgecolor="black", linewidth=0.5
-)
-ax_alaska.set_xlim(-200, -100)
-ax_alaska.set_ylim(50, 73)
+plot_with_legend(alaska, ax_alaska, xlim=(-200, -100), ylim=(50, 73))
 
 # Hawaii plot in the second row, second column
 ax_hawaii = plt.subplot2grid((2, 2), (1, 1), fig=fig)
-
-# Plot Hawaii in the subplot
-hawaii.plot(
-    ax=ax_hawaii, column=column_to_plot, cmap=cmap, edgecolor="black", linewidth=0.5
-)
-ax_hawaii.set_xlim(-162, -152)
-ax_hawaii.set_ylim(18, 24)
+plot_with_legend(hawaii, ax_hawaii, xlim=(-162, -152), ylim=(18, 24))
 
 # Annotate the states
 annotate_states(contiguous_us, ax_main, value_col=column_to_plot)
 annotate_states(alaska, ax_alaska, value_col=column_to_plot)
 annotate_states(hawaii, ax_hawaii, value_col=column_to_plot)
 
+
 for ax in fig.axes:
     ax.set_axis_off()
 
-turn_on_spines(ax_hawaii, spine_width=1)
-turn_on_spines(ax_alaska, spine_width=1)
+set_background_color(ax_hawaii)
+set_background_color(ax_alaska)
 
-# Tight layout to ensure subplots fit well within the figure
-# plt.tight_layout()
-plt.subplots_adjust(hspace=0.05)
+
+legend_handles = [
+    mpatches.Patch(color=color, label=label) for label, color in color_mapping.items()
+]
+
+fig.legend(
+    handles=legend_handles,
+    title="Employment growth (APC)",
+    loc="lower center",
+    bbox_to_anchor=(
+        0.5,
+        -0.03,
+    ),  # Position the legend at the bottom center of the figure
+    ncol=len(color_mapping),  # Arrange items in a single row
+    frameon=False,
+)
+
+# Adjust plot layout
+plt.subplots_adjust(hspace=0.06)
 plt.savefig("test", dpi=300, bbox_inches="tight")
 plt.show()
 
@@ -155,8 +185,6 @@ plt.show()
 # ToDO:
 # annotate extreme values only - add a annotate function that finds outliers.
 # Add title for Hawaii and Alaska inside plot
-# add cmap legend
 # add title and sub title
 # add source
 # make all lines of code commented
-# How to add multiple choropleth maps to show different years.
